53eee51ddc564d6ac9a12881e1a9ef87c9d5b293
Vincenzo Governali <vincigov99@gmail.com>
2022-01-07 12:15:35 +0100

    Aggiungere i file di progetto.

---
+++ FFmpeg Recorder/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+
+namespace FFmpeg_Recorder
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void record(object sender, RoutedEventArgs e)
+        {
+            String urlText = url.Text;
+            if(urlText.Length > 0 && Uri.IsWellFormedUriString(urlText, UriKind.Absolute))
+            {
+
+                int time = int.Parse(chron.Text);
+
+                
+                Process proc = new Process();
+                proc.StartInfo.FileName = "ffmpeg.exe";
+                proc.StartInfo.Arguments = " -i \"" + urlText +"\" output.mp4";
+                proc.StartInfo.UseShellExecute = true;
+                proc.Start();
+                if (time > 0)
+                {
+                    Thread thread = new Thread(()=>chronStop(time * 60000, proc));
+                    thread.Start();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Url non valido!", "Errore!");
+            }
+        }
+
+
+        private void chronStop(int sleep, Process proc)
+        {
+            Thread.Sleep(sleep);
+            proc.Kill();
+        }
+
+        private void insertTime(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(chron.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Inserisci un numero!");
+                chron.Text = chron.Text.Remove(chron.Text.Length - 1);
+            }
+        }
+    }
+}
